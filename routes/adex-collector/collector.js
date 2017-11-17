e47054a709627d75627a325f2d154f8ed7e4a092
Shteryana Shopova <s.shopova@strem.io>
2017-11-17 17:15:01 +0200

    Do not distinguish between event types Route ednpoint under /events Summary stats per 60 second intervals which expire after 1 month

--- routes/adex-collector/collector.js
+++ routes/adex-collector/collector.js
@@ -12,15 +12,12 @@ const pid = process.pid;
 
 var redisClient = null;
 var scriptManager = null;
-var endpoints = ['impression', 'click', 'leave'];
-
+var EXPIRY_INTERVAL = 2678400;
+var MSECS_IN_SEC = 1000;
 
 redisInit();
 redisLoadScript();
-
-endpoints.forEach(function (element, index) {
-    registerEndpoint(index);
-});
+registerEndpoint();
 
 function redisInit() {
     redisClient = redis.createClient();
@@ -37,15 +34,15 @@ function redisLoadScript() {
     scriptManager.loadFromFile('timefilter', './zcount.lua');
 }
 
-function registerEndpoint(which) {
-    console.log('[' + pid + '] ' + 'Register endpoint ' + which + ' /' + endpoints[which]);
-    router.get('/' + endpoints[which], function (request, response) {
+function registerEndpoint() {
+    console.log('[' + pid + '] ' + 'Register endpoint /events');
+    router.get('/events', function (request, response) {
         var whenStart = Date.now();
         var bid = JSON.parse(request.query.bid);
         // console.log('Received endpoint request, data ' + endpoints[which] +
         // ' start at ' + request.query.start + ' end at ' + request.query.end);
         if (request.query.start === undefined && request.query.end === undefined) {
-            redisClient.zcard([bid + ':' + endpoints[which]], (err, result) => {
+            redisClient.zcard(['bid:' + bid], (err, result) => {
                 if (err)
                     throw err;
                 var whenEnd = Date.now();
@@ -59,7 +56,7 @@ function registerEndpoint(which) {
             if (request.query.end === undefined)
                 request.query.end = Date.now();
             var keys = [request.query.start, request.query.end];
-            var values = [bid + ':' + endpoints[which]];
+            var values = ['bid:' + bid];
             // console.log('scriptManager.run to run keys ' + keys + ' values ' + values);
             scriptManager.run('timefilter', keys, values, function (err, result) {
                 if (err) {
@@ -76,12 +73,29 @@ function registerEndpoint(which) {
 }
 
 function submitEntry(payload, response) {
-    redisClient.zadd([payload.bid + ':' + payload.type, Date.parse(payload.time),
-    'uid:' + payload.uid + ' adunit:' + payload.adunit], (err, result) => {
-        if (err || result < 1) {
-            console.log('Add entry failed (' + result + ') ' + err);
+    redisClient.zadd(['bid:' + payload.bid, Date.parse(payload.time),
+    'type:'+ payload.type + ' uid:' + payload.uid + ' adunit:' + payload.adunit], (err, result) => {
+        if (err) {
+            console.log('[zadd] Add entry failed (' + result + ') ' + err);
         }
         response.redirect('/');
+    });
+    var timeInterval = Math.floor(Date.parse(payload.time) / (60 * MSECS_IN_SEC));
+    redisClient.hincrby(['timeinterval:' + payload.bid, timeInterval, 1], (err, result) => {
+        if (err) {
+            console.log('[HINCRBY] Add entry failed (' + timeInterval + ') ' + err);
+        } else {
+            if (result < 2) {
+                var date = new Date();
+                var expiryTime = (timeInterval + 1 ) * 60 * MSECS_IN_SEC +
+                    EXPIRY_INTERVAL * MSECS_IN_SEC - date.getTime();
+                var expirySeconds = Math.floor(expiryTime / 1000);
+                redisClient.expire(['timeinterval:' + payload.bid, expirySeconds], (err, res) => {
+                    if (err)
+                        console.log('[EXPIRE] set entry expiry time failed ' + err);
+                });
+            }
+        }
     });
 }
 
