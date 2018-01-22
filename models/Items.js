8f2a9b1356f5888258ac58150b7d3a5fcf9b2ee7
Ivo paunov <ivo.paunov@gmail.com>
2018-01-22 18:47:05 +0200

    add to ipfs onlu adUnit and adSlot

--- models/Items.js
+++ models/Items.js
@@ -3,46 +3,55 @@
 const db = require('./../mongoConnection').getDb()
 const collection = db.collection('items')
 const ipfs = require('./../services/ipfs/ipfs')
+const constants = require('adex-constants')
 
 class Items {
     addItem(item, user) {
-        return new Promise((resolve, reject) => {
-            let createdOn = Date.now()
-            let ipfsMeta = item._meta
-            ipfsMeta.createdOn = createdOn
-            ipfsMeta.owner = user
+        let createdOn = Date.now()
+        let ipfsMeta = item._meta
+        ipfsMeta.createdOn = createdOn
+        ipfsMeta.owner = user
 
+        if (constants.ItemIpfsByType) {
             ipfs.addFileToIpfs(JSON.stringify(ipfsMeta))
                 .then((itemIpfs) => {
-                    let sizeAndType = 0
-
-                    if (item._meta.adType && item._meta.size) {
-                        // TODO: potential bug if more than 9 ad types
-                        sizeAndType = parseInt(item._meta.adType + '' + item._meta.size)
-                    }
-
-                    let dbItem = {
-                        type: item._type, //unit / slot 
-                        user: user,
-                        _description: item._description, //Field only users info not in ipfs
-                        _meta: ipfsMeta, // the _meta as in ipfs
-                        _ipfs: itemIpfs,
-                        sizeAndType: sizeAndType,
-                        _createdOn: createdOn,
-                        _modifiedOn: undefined,
-                        _deleted: false,
-                        _archived: false
-                    }
-
-                    collection.insertOne(dbItem, (err, result) => {
-                        if (err) {
-                            console.log('insertOne err', err)
-                            return reject(err)
-                        }
-
-                        return resolve(dbItem)
-                    })
+                    return this.addItemToDb({ user: user, item: item, meta: ipfsMeta, itemIpfs, createdOn: createdOn })
                 })
+        } else {
+            return this.addItemToDb({ user: user, item: item, meta: ipfsMeta, createdOn: createdOn })
+        }
+    }
+
+    addItemToDb({ user, item, meta, ipfs = '', createdOn }) {
+        return new Promise((resolve, reject) => {
+            let sizeAndType = 0
+
+            if (item._meta.adType && item._meta.size) {
+                // TODO: potential bug if more than 9 ad types
+                sizeAndType = parseInt(item._meta.adType + '' + item._meta.size)
+            }
+
+            let dbItem = {
+                type: item._type, //unit / slot 
+                user: user,
+                _description: item._description, //Field only users info not in ipfs
+                _meta: meta, // the _meta as in ipfs
+                _ipfs: ipfs,
+                sizeAndType: sizeAndType,
+                _createdOn: createdOn || Date.now(),
+                _modifiedOn: undefined,
+                _deleted: false,
+                _archived: false
+            }
+
+            collection.insertOne(dbItem, (err, result) => {
+                if (err) {
+                    console.log('insertOne err', err)
+                    return reject(err)
+                }
+
+                return resolve(dbItem)
+            })
         })
     }
 
