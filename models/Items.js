4b259eee9c32e365e4a19f51c069225fbc4cf67a
Ivo paunov <ivo.paunov@gmail.com>
2018-01-22 16:06:12 +0200

    fix  item model and indexing

--- models/Items.js
+++ models/Items.js
@@ -2,52 +2,70 @@
 
 const db = require('./../mongoConnection').getDb()
 const collection = db.collection('items')
+const ipfs = require('./../services/ipfs/ipfs')
 
 class Items {
-    addItem(item, ipfs, user) {
+    addItem(item, user) {
         return new Promise((resolve, reject) => {
-            let sizeAndType = 0
+            let createdOn = Date.now()
+            let ipfsMeta = item._meta
+            ipfsMeta.createdOn = createdOn
+            ipfsMeta.owner = user
 
-            if (item._meta.adType && item._meta.size) {
-                sizeAndType = parseInt(item._meta.adType + item._meta.size)
-            }
+            ipfs.addFileToIpfs(JSON.stringify(ipfsMeta))
+                .then((itemIpfs) => {
+                    let sizeAndType = 0
 
-            let dbItem = {
-                type: item._type, //unit / slot 
-                user: user,
-                description: item._description || item._meta.description, //Field only users info not in ipfs
-                itemObj: item,
-                ipfs: ipfs,
-                sizeAndType: sizeAndType || 0
-                //TODO: indexing, createdon, updatedon etc.
-            }
+                    if (item._meta.adType && item._meta.size) {
+                        // TODO: potential bug if more than 9 ad types
+                        sizeAndType = parseInt(item._meta.adType + '' + item._meta.size)
+                    }
 
-            collection.insertOne(dbItem, (err, result) => {
-                if (err) {
-                    console.log('insertOne err', err)
-                    return reject(err)
-                }
+                    let dbItem = {
+                        type: item._type, //unit / slot 
+                        user: user,
+                        _description: item._description, //Field only users info not in ipfs
+                        _meta: ipfsMeta, // the _meta as in ipfs
+                        _ipfs: itemIpfs,
+                        sizeAndType: sizeAndType,
+                        _createdOn: createdOn,
+                        _modifiedOn: undefined,
+                        _deleted: false,
+                        _archived: false
+                    }
 
-                let responseItem = { ...result.ops[0].itemObj }
-                responseItem._id = result.ops[0]._id
+                    collection.insertOne(dbItem, (err, result) => {
+                        if (err) {
+                            console.log('insertOne err', err)
+                            return reject(err)
+                        }
 
-                return resolve(responseItem)
-
-            })
+                        return resolve(dbItem)
+                    })
+                })
         })
     }
 
     getUserItems(user, type) {
         return new Promise((resolve, reject) => {
 
-            collection.find({ user: user, type: parseInt(type) }).project({ itemObj: 1, description: 1 }).toArray((err, result) => {
-                if (err) {
-                    console.log('find items err', err)
-                    return reject(err)
-                }
+            collection.find({ user: user, type: parseInt(type), _deleted: false })
+                .project({
+                    type: 1,
+                    _description: 1,
+                    _meta: 1,
+                    _ipfs: 1,
+                    _createdOn: 1,
+                    _archived: 1
+                })
+                .toArray((err, result) => {
+                    if (err) {
+                        console.log('find items err', err)
+                        return reject(err)
+                    }
 
-                return resolve(result)
-            })
+                    return resolve(result)
+                })
         })
     }
 
