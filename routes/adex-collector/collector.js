fb5af0b44d872e3b6cc64ddb101e0884d7251a68
Shteryana Shopova <s.shopova@strem.io>
2018-02-07 15:47:46 +0200

    Remove dead code

--- routes/adex-collector/collector.js
+++ routes/adex-collector/collector.js
@@ -16,24 +16,9 @@ const pid = process.pid;
 var scriptManager = null;
 var EXPIRY_INTERVAL = 2678400;
 var MSECS_IN_SEC = 1000;
-// var dbPort = process.env.DBPORT || 6379;
-// var dbHost = process.env.DBHOST || '127.0.0.1';
 
-// redisInit();
 redisLoadScript();
 registerEndpoint();
-
-console.log('Database server is on ' + dbHost + ', port ' + dbPort);
-
-// function redisInit() {
-//     redisClient = redis.createClient(dbPort, dbHost);
-//     redisClient.on('ready', function () {
-//         console.log('Redis is ready');
-//     });
-//     redisClient.on('error', function () {
-//         process.exit(1);
-//     });
-// }
 
 function redisLoadScript() {
     scriptManager = new scripto(redisClient);
