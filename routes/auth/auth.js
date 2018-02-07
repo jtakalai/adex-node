edcdd01d5faecfce8d6cf2f56d60235d0160362a
Ivo paunov <ivo.paunov@gmail.com>
2018-02-07 16:04:05 +0200

    add sign types switch

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -6,17 +6,57 @@ const Web3 = require('web3')
 var redisClient = require('./../../redisInit')
 const router = express.Router()
 let web3 = new Web3()
+let { SIGN_TYPES } = require('adex-constants').exchange
+
+const authPersonal = ({ signature, authToken, userid }) => {
+    let error = null,
+        user
+
+    try {
+        user = web3.eth.accounts.recover(web3.eth.accounts.hashMessage(authToken), signature)
+    } catch (err) {
+        error = err
+    }
+
+    return {
+        user: user,
+        err: error
+    }
+}
 
 router.post('/auth', (req, res) => {
     var userid = req.query.userid,
         signature = req.query.signature,
-        authToken = req.query.authToken
+        authToken = req.query.authToken,
+        sigMode = parseInt(req.query.mode),
+        err = null,
+        user = null,
+        authRes = {}
+
+    switch (sigMode) {
+
+        case SIGN_TYPES.Personal.id:
+            authRes = authPersonal({ signature: signature, authToken: authToken, userid: userid })
+            break
+        case SIGN_TYPES.MetamaskTyped.id:
+            // Auth MetamaskTyped
+            break
+        case SIGN_TYPES.Trezor.id:
+            // Auth Trezor
+            break
+        case SIGN_TYPES.Ledger.id:
+            // Auth Ledger
+            break
+
+        default:
+            break
+    }
+
+    err = authRes.err
+    user = authRes.user
 
     // console.log('User id ' + userid + ', token ' + authToken + ' signature ' + signature)
-
-    try {
-        var user = web3.eth.accounts.recover(web3.eth.accounts.hashMessage(authToken), signature)
-    } catch (err) {
+    if (err) {
         console.log('Error verifying signature ' + err)
         res.status(401).send('Error verifying signature ' + err)
         return
