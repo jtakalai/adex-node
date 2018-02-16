fa1c86789e0ba6d14edf00736fd1a63e224bccd1
Ivo paunov <ivo.paunov@gmail.com>
2018-02-16 16:48:15 +0200

    add adex smart contracts

---
+++ services/web3/ADX.js
@@ -0,0 +1,43 @@
+const Web3 = require('web3')
+
+const tokenAbi = require('./abi/ADXToken')
+const exchangeAbi = require('./abi/ADXExchange')
+const web3Utils = Web3.utils
+
+const testrpcCfg = {
+    node: 'https://ropsten.infura.io/metamask',
+    addr: {
+        token: '0xD06632e3916776e781d66A7A08ffBb77271742F7',
+        exchange: '0x18d08c0e62523cD613F16d1ba9Eb98666Aeb45E8',
+    }
+}
+
+const mainnetCfg = {
+    node: 'https://parity.wings.ai',
+    addr: {
+        token: '0x4470BB87d77b963A013DB939BE332f927f2b992e',
+        exchange: '0x0f6029ebde2ecd9ab4d60dd5d0a297e9e59bf77a', // TODO:
+    }
+}
+
+let cfg
+
+if (process.env.NODE_ENV === 'production') {
+    cfg = mainnetCfg
+} else {
+    cfg = testrpcCfg
+}
+
+let provider = new Web3.providers.HttpProvider(cfg.node)
+let web3 = new Web3(provider)
+
+let token = new web3.eth.Contract(tokenAbi, cfg.addr.token)
+let exchange = new web3.eth.Contract(exchangeAbi, cfg.addr.exchange)
+
+module.exports = {
+    web3: web3,
+    cfg: cfg,
+    token: token,
+    exchange: exchange,
+    web3Utils: web3Utils
+}
