4b82f0de535f22b19905d1a34f7fe4279f99a570
Shteryana Shopova <s.shopova@strem.io>
2018-02-19 19:27:31 +0200

    Add redis password authentication

--- redisInit.js
+++ redisInit.js
@@ -1,8 +1,13 @@
 const redis = require('redis')
-var dbPort = process.env.DBPORT || 6379;
-var dbHost = process.env.DBHOST || '127.0.0.1';
+var dbPort = process.env.REDIS_PORT || 6379;
+var dbHost = process.env.REDIS_HOST || '127.0.0.1';
+var dbPassword = process.env.REDIS_PASSWD || 'zo4ohJao0yaav4Xah8oogiqu1Johkeig';
 
 const redisClient = redis.createClient(dbPort, dbHost)
+
+redisClient.auth(dbPassword, function (err) {
+    if (err) throw err;
+});
 
 console.log('Redis Database server is on ' + dbHost + ', port ' + dbPort);
 
