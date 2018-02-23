406db781c9ac17a5fed733882d19be18e7d176a6
Ivo <ivo.paunov@gmail.com>
2018-02-24 01:39:47 +0200

    auth-check new resp

--- routes/auth/auth-check.js
+++ routes/auth/auth-check.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.get('/auth-check', (req, res) => {
     if (req.user) {
-        res.send(true)
+        res.send(JSON.stringify({ authenticated: true }))
     } else {
         res.status(403).send({ error: 'Authentication required' })
     }
